3a95b4defe0eb3ec90ec7429ccd477fb8e66c02d
Gastón Saillén <gsaillen95@gmail.com>
2021-01-03 17:37:54 -0300

    Room Entidades

--- app/src/main/java/com/example/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -1,5 +1,9 @@
 package com.example.movieapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 data class Movie(
     val id: Int = -1,
     val adult: Boolean = false,
@@ -19,3 +23,35 @@ data class Movie(
 
 
 data class MovieList(val results: List<Movie> = listOf())
+
+//Room
+
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo(name = "adult")
+    val adult: Boolean = false,
+    @ColumnInfo(name = "backdrop_path")
+    val backdrop_path: String = "",
+    @ColumnInfo(name = "original_title")
+    val original_title: String = "",
+    @ColumnInfo(name = "original_language")
+    val original_language: String = "",
+    @ColumnInfo(name = "overview")
+    val overview: String = "",
+    @ColumnInfo(name = "popularity")
+    val popularity: Double = -1.0,
+    @ColumnInfo(name = "poster_path")
+    val poster_path: String = "",
+    @ColumnInfo(name = "release_date")
+    val release_date: String = "",
+    @ColumnInfo(name = "title")
+    val title: String = "",
+    @ColumnInfo(name = "video")
+    val video: Boolean = false,
+    @ColumnInfo(name = "vote_average")
+    val vote_average: Double = -1.0,
+    @ColumnInfo(name = "vote_count")
+    val vote_count: Int = -1
+)
